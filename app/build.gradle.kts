ede5f4477b9f13ce64da229bcb5661a4698ec978
Jemutai <janetjemutai19@gmail.com>
2024-05-28 17:18:24 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.retrofit)
+    implementation (libs.converter.moshi)
+    implementation (libs.okhttp)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
 }
